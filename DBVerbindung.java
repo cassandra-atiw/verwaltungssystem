b888d4f2c0322930fc9184f14fc3b08321c51377
cassandra-atiw <tschaefer.home@outlook.de>
2017-05-19 11:34:36 +0200

    Update DBVerbindung.java

--- DBVerbindung.java
+++ DBVerbindung.java
@@ -12,23 +12,22 @@ import com.datastax.driver.core.*;
  * @author timon
  */
 public class DBVerbindung {
-        
-        
          public final static Cluster cl = Cluster.builder().addContactPoint("127.0.0.1").withPort(9042).build(); 
          public static Session s;
          
-         public static ResultSet select(String query) throws Exception
-         {
-             s=cl.connect("Sitzplatzverteilung");
-             ResultSet rs=s.execute(query);
-             s.close();
-             return rs;
+         public static void openSession(){
+             s = cl.connect("Sitzplatzverteilung");
          }
          
-         public static void  insert(String query) throws Exception
-         {
-                   s=cl.connect("Sitzplatzverteilung");
-                   s.execute(query);  
-                   s.close();
+         public static void closeSession(){
+             s.close();
+         }
+         
+         public static ResultSet select(String query) throws Exception{
+            return s.execute(query);
+         }
+         
+         public static void insert(String query) throws Exception{
+            s.execute(query);  
          }
 }
